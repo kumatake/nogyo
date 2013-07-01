fe55b75f45d2d4211dadc3477c7df04900784b94
kumatake <kamatuka3@gmail.com>
2013-07-01 13:44:42 +0900

    テストファイルの追加

---
+++ gpio_input.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+# -*- config: utf-8 -*-
+ 
+import RPi.GPIO as GPIO
+import time
+ 
+IO_NO = 4
+ 
+print("press ^C to exit program ...\n")
+ 
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(IO_NO, GPIO.IN)
+ 
+try:
+ while True:
+  print(GPIO.input(IO_NO))
+  time.sleep(1)
+except KeyboardInterrupt:
+ print("detect key interrupt\n")
+ 
+GPIO.cleanup()
+print("Program exit\n")
---
+++ gpio_led_blink.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+# -*- config: utf-8 -*-
+ 
+import RPi.GPIO as GPIO
+import time
+ 
+# 使うGPIOピンの番号
+IO_NO = 4
+ 
+print("press ^C to exit program ...\n")
+ 
+# GPIOピン番号を用いる
+GPIO.setmode(GPIO.BCM)
+# 基板上のピン番号を用いる
+#GPIO.setmode(GPIO.BOARD)
+ 
+GPIO.setup(IO_NO, GPIO.OUT)
+ 
+try:
+ while True:
+  GPIO.output(IO_NO, True)
+  time.sleep(0.5)
+  GPIO.output(IO_NO, False)
+  time.sleep(0.5)
+except KeyboardInterrupt:
+ print("detect key interrupt\n")
+ 
+GPIO.cleanup()
+print("Program exit\n")
