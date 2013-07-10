7b92cc90d0cf788b15ebb0894d036d3e86d01869
kumatake <kamatuka3@gmail.com>
2013-07-10 14:15:54 +0900

    ledtest.php追加

---
+++ ledtest.php
@@ -0,0 +1,13 @@
+<<html>
+<head>
+	<title>LEDpython呼び出しテスト</title>
+</head>
+<body>
+	<?php
+		if($_REQUEST[start] == TRUE){
+			echo "<P>hoge</P>";
+		}
+	?>
+	<input name="startbutton" type="start" value="LED点灯！">
+</body>
+</html>>
